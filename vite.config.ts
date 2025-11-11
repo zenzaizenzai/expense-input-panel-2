e5794a8709794c2b5d2bf43c609b8f8e4c9f7a76
zenzai <zenzaizenzai@outlook.com>
2025-11-11 19:34:05 +0900

    feat: setup GitHub Actions and base path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+	// ★★★ この行を追加 ★★★
+    base: '/expense-input-panel-2/', 
+    // ★★★★★★★★★★★★★★★★
       server: {
         port: 3000,
         host: '0.0.0.0',
